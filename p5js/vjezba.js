3a3f31862a69db6a4988c5dc6d6297c8c3e7ce81
TinoZg <42203467+TinoZg@users.noreply.github.com>
2018-08-14 11:52:55 +0200

    Add files via upload

---
+++ p5js/vjezba.js
@@ -0,0 +1,39 @@
+let info;
+let url = 'http://api.openweathermap.org/data/2.5/weather?q=Zagreb&APPID=d5da566641898b814482126740a1971f&units=metric';
+let temp;
+let city;
+let date;
+let x;
+let humidity;
+let weathericon;
+
+function setup(){
+  noCanvas();
+  loadJSON(url, getData);
+}
+
+function getData(data){
+  //info = data;
+  console.log(data.main.temp);
+  console.log(data.name);
+  console.log(data.main.humidity);
+  console.log(data.main.pressure);
+  temp = select('#temperature');
+  temp.html(data.main.temp);
+  city = select('#location');
+  city.html(data.name);
+  getDate();
+  date = select('#date');
+  date.html(x);
+  humidity = select('#humidity');
+  humidity.html(data.main.humidity);
+  pressure = select('#pressure');
+  pressure.html(data.main.pressure);
+}
+
+function getDate(){
+  let d = day();
+  let m = month();
+  let y = year();
+  x = d + '.' + m + '.' + y;
+}
